d5e54e03f68fc7462c0643efedf140f1a152e40e
josephheinz <josephheinz28@gmail.com>
2026-06-10 14:14:07 -0400

    fix indenting

--- internal/ui/cpu.go
+++ internal/ui/cpu.go
@@ -150,7 +150,8 @@ func (v *cpuView) bottomRow() fyne.CanvasObject {
 		return perCore
 	}
 	procs := newPanel(labelTopCPUProcesses, newJumpLink(labelAllProcessesLink), v.table)
-	return newWeightedHBox(tabPad,
+	return newWeightedHBox(
+		tabPad,
 		weightedPane{object: perCore, weight: perCorePaneWeight},
 		weightedPane{object: procs, weight: processPaneWeight},
 	)
